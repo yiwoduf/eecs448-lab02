b6afc6d91e2328c9063d170e0a955293a5b02f5d
Peter Jaeyol Lee <jylee@ku.edu>
2022-02-03 10:45:33 -0600

    search() function implemented

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -37,9 +37,14 @@ bool LinkedList<T>::search(T value) const
 	Node<T> *temp = m_front;
 	bool isFound = false;
 
-	/** TODO
-		Fix this method
-	*/
+	for (int i = 0; i < m_size; i++)
+	{
+		if (temp->getValue() == value)
+		{
+			isFound = true;
+		}
+		temp = temp->getNext();
+	}
 
 	return (isFound);
 }
